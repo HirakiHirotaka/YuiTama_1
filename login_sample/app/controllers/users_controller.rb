7c8acfee84911661493181317f369f7a183cdbca
HirakiHirotaka <e155711@ie.u-ryukyu.ac.jp>
2018-01-10 13:03:39 +0900

    つかちゃんのlogin機能ファイルを追加

---
+++ login_sample/app/controllers/users_controller.rb
@@ -0,0 +1,41 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(name: params[:name],email: params[:email])
+    if @user.save then
+      flash[:notice] = "your register success."
+      redirect_to("/users/#{@user.id}")
+    else
+      render("/users/new")
+    end
+  end
+
+  def edit
+    @user = User.find_by(id: params[:id])
+  end
+
+  def update
+    @user = User.find_by(id: params[:id])
+    @user.name = params[:name]
+    @user.email = params[:email]
+    if @user.save then
+      flash[:notice] = "edited"
+      redirect_to "/users/#{@user.id}/"
+    else
+      render("/users/edit")
+    end
+  end
+
+
+end
