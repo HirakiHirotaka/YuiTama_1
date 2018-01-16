4b257da99b784579d8ed9a1130c9f36c87e20cb7
HirakiHirotaka <e155711@ie.u-ryukyu.ac.jp>
2018-01-16 14:50:31 +0900

    つかちゃんのlogin機能

--- login_sample/app/controllers/users_controller.rb
+++ login_sample/app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user,{only:[:edit,:update,:index,:show]}
+  before_action :forbid_login_user, {only: [:new,:create,:login_form,:login]}
+  before_action :ensure_correct_user,{only:[:edit,:update]}
+
   def index
     @users = User.all
   end
@@ -9,10 +13,13 @@ class UsersController < ApplicationController
 
   def new
     @user = User.new
+    if @user.save
+      session[:user_id] = user.id
+    end
   end
 
   def create
-    @user = User.new(name: params[:name],email: params[:email])
+    @user = User.new(name: params[:name],email: params[:email],image_name:"default_egashira.jpg",password: params[:password])
     if @user.save then
       flash[:notice] = "your register success."
       redirect_to("/users/#{@user.id}")
@@ -37,5 +44,35 @@ class UsersController < ApplicationController
     end
   end
 
+  def login_form
+
+  end
+
+  def login
+    @user = User.find_by(email: params[:email],password: params[:password])
+    if @user
+      session[:user_id] = @user.id
+      flash[:notice] = "logged inn , Welcome!"
+      redirect_to("/users/index")
+    else
+      @error_message = "Wrong emailaddress or Password!!"
+      @email = params[:email]
+      @password = params[:password]
+      render("users/login_form")
+      end
+  end
+
+  def logout
+      session[:user_id] = nil
+      flash[:notice] = "logged out , see you later!"
+      redirect_to("/login")
+  end
+
+  def ensure_corrent_user
+    if @current_user != params[:id].to_i
+      flash[:notice] = "permission denied!!"
+      redirect_to("/users/index")
+    end
+  end
 
 end
--- login_sample/db/schema.rb
+++ login_sample/db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180109083125) do
+ActiveRecord::Schema.define(version: 20180110132755) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_name"
+    t.string "password"
   end
 
 end
